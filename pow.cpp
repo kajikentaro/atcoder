2342a586c774e0886e163d19732e71b57095b39f
kajikentaro <k.kentarou0412@gmail.com>
2021-12-18 14:37:59 +0000

    add: pow

---
+++ pow.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+#define rep(x,y) for(int x=0;x<y;x++)
+#define rrep(x,y) for(int x=y-1;x>=0;x--)
+#define orep(x,y) for(int x=1;x<=y;x++)
+#define ll long long
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+using namespace std;
+using namespace atcoder;
+using mint = modint1000000007;
+using P = pair<int,int>;
+mint mypow(mint a, int b){
+	if(b == 0)return 1;
+	mint res = 1;
+	if(b % 2 == 1){
+		res *= a;
+		b--;
+	}
+	mint k = mypow(a, b/2);
+	res *= k;
+	res *= k;
+	return res;
+
+}
