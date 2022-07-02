7dc6146cbb425ea0ec5fb2ddb3fac9cdfbc5b988
kajikentaro <k.kentarou0412@gmail.com>
2022-07-02 17:01:51 +0900

    add

--- icpc/2017/c.cpp
+++ icpc/2017/c.cpp
@@ -17,67 +17,67 @@ using namespace std;
 using namespace atcoder;
 using mint = modint1000000007;
 using P = pair<int, int>;
-struct Point {
-  int h, w;
-};
 
 int h, w;
 vector<vector<int>> grid;
-vector<vector<int>> used_global;
-vector<vector<int>> used;
-int wall_min;
-void dfs(int level, Point now) {
-  const Point dirs[] = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
-  if (used[now.h][now.w]) return;
-  used[now.h][now.w] = 1;
-  for (auto d : dirs) {
-    Point next = now;
-    next.h += d.h;
-    next.w += d.w;
-    if (next.h < 0 || next.w < 0 || next.h >= h || next.w >= w) continue;
-    if (grid[next.h][next.w] > level) {
-      chmin(wall_min, grid[next.h][next.w]);
-      continue;
-    }
-    dfs(level, next);
-  }
-}
 
-ll injection(Point start) {
-  used.assign(h, vector<int>(w));
-  int level = grid[start.h][start.w];
-  wall_min = 1e9;
-  dfs(level, start);
+ll injection(int h_min, int h_max, int w_min, int w_max) {
+  int wall_min = 1e9;
+  int in_max = 0;
 
-  ll bonus = 0;
-  ll used_block = 0;
+  int size = 0;
 
+  ll height_sum = 0;
   rep(hi, h) rep(wi, w) {
-    if (!used[hi][wi]) continue;
-    used_block++;
-    bool is_edge = (hi == 0 || wi == 0 || hi == h - 1 || wi == w - 1);
-    if (is_edge && used[hi][wi]) return 0;
-    bonus += level - grid[hi][wi];
+    auto is_in = [&]() {
+      return h_min < hi && hi < h_max && w_min < wi && wi < w_max;
+    };
+    auto is_out = [&]() {
+      return hi < h_min || h_max < hi || wi < w_min || w_max < wi;
+    };
+    auto is_wall = [&]() {
+      if (is_in()) return false;
+      if (is_out()) return false;
+      return true;
+    };
+    if (is_wall()) {
+      // wall
+      chmin(wall_min, grid[hi][wi]);
+    }
+    if (is_in()) {
+      // in wall
+      chmax(in_max, grid[hi][wi]);
+      height_sum += grid[hi][wi];
+      size++;
+    }
   }
+  if (in_max >= wall_min) return 0;
+  if (size == 0) return 0;
 
-  ll base_score = (wall_min - level) * used_block;
-  ll ans = bonus + base_score;
+  ll ans = size * wall_min;
+  ans -= height_sum;
   return ans;
 }
 
 bool func() {
   cin >> h >> w;
-  grid.resize(h, vector<int>(w));
-  used_global.resize(h, vector<int>(w));
+  if (h + w == 0) return false;
+  grid.assign(h, vector<int>(w));
   rep(i, h) rep(j, w) cin >> grid[i][j];
 
   ll ans = 0;
-  rep(hi, h) rep(wi, w) {
-    if (hi == 0 || wi == 0 || hi == h - 1 || wi == w - 1) continue;
-    if (used_global[hi][wi]) continue;
-    ll s_ans = injection(Point{hi, wi});
-    chmax(ans, s_ans);
+
+  for (int h_min = 0; h_min < h; h_min++) {
+    for (int h_max = h_min + 1; h_max < h; h_max++) {
+      for (int w_min = 0; w_min < w; w_min++) {
+        for (int w_max = w_min + 1; w_max < w; w_max++) {
+          ll s_ans = injection(h_min, h_max, w_min, w_max);
+          chmax(ans, s_ans);
+        }
+      }
+    }
   }
+
   cout << ans << endl;
   return true;
 }
